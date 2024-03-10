61f322fa2d9a9ed2e9550f56d826ce9bc0dac3f7
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-03-10 11:01:08 +0530

    first commit

---
+++ 2.py
@@ -0,0 +1,11 @@
+a=20 or 40
+if 30 <= a <=50:
+    print('Hello')
+else:
+    print('Hi')
+
+# Or condition takes the first truthy value.
+# Here a takes 20
+# a=20 doesn't meet the range in if condition
+# Else Block is executed
+# Hi is the output
